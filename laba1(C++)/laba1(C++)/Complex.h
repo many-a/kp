fc3e8005540226d818ccbd61c579562309891022
many-a <91227545+many-a@users.noreply.github.com>
2025-01-28 19:45:21 +1000

    Delete laba1(C++) directory

--- laba1(C++)/laba1(C++)/Complex.h
+++
@@ -1,6 +0,0 @@
-#ifndef COMPLEX_H
-#define COMPLEX_H
-
-int Complex(double z1, double z2);
-
-#endif 
--- laba1(C++)/laba1(C++)/Descriminant.h
+++
@@ -1,6 +0,0 @@
-#ifndef DESCRIMINANT_H
-#define DESCRIMINANT_H
-
-int Descriminant(double a, double b, double c);
-
-#endif 
